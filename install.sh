26110784bee31d51824c3e9a4474c58068ae14fe
Fips11 <nephilim11@hotmail.de>
2020-02-23 21:32:07 +0000

    complete install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
@@ -5,37 +7,47 @@ fi
 
 #/boot/config.txt
 
-if ! grep -q 'dtoverlay=dwc2' /boot/config.txt ; then
+#if ! grep -q 'dtoverlay=dwc2' /boot/config.txt ; then
 
-    echo "change /boot/config.txt"
-    echo "dtoverlay=dwc2" >> /boot/config.txt
-fi
+#    echo "change /boot/config.txt"
+#    echo "dtoverlay=dwc2" >> /boot/config.txt
+#fi
 
 #/etc/modules
 
-if ! grep -q 'dwc2' /etc/modules ; then
+#if ! grep -q 'dwc2' /etc/modules ; then
 
-    echo "delete dwc2 in /etc/modules" ###falsch?
-    sed -i '/^dwc2/d' /etc/modules
-fi
+#    echo "delete dwc2 in /etc/modules" ###falsch?
+#    sed -i '/^dwc2/d' /etc/modules
+#fi
 
 #/etc/sudoers
 
 #line="#includedir /etc/sudoers.d"
-
+#
 #if ! grep -q "${line}" /etc/sudoers ; then
 #  sudo sed '$i'"${line}"'' /etc/sudoers | sudo EDITOR='tee -a' visudo
 #fi
 
 
 #add save control script to /usr/sbin/
-echo "add script to /usr/sbin/"
-new_script=""
+echo "add flash script to /usr/sbin/"
+#new_script=""
+#echo "$new_script" > /etc/sbin/control-flash-host
 
-echo "$new_script" > /etc/sbin/control-flash-host
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+chmod +x /usr/sbin/flash
+
+cp "$DIR/flash" "/usr/sbin/"
 
 echo "add control-flash-host to sudoers"
 #add own file to /etc/sudoers.d 
-own_sudoers="pi ALL=NOPASSWD: /usr/sbin/control-flash-host * *"
+own_sudoers="pi ALL=NOPASSWD: /usr/sbin/flash * *"
 
+if  grep  -q "$own_sudoers" /etc/sudoers.d/sla_plugin ; then
+    echo "/etc/sudoers.d/sla_plugin already existing"
+    exit 0    
+fi
+
+echo "create File: /etc/sudoers.d/sla_plugin"
 echo "$own_sudoers" | sudo EDITOR='tee -a' visudo -f /etc/sudoers.d/sla_plugin
